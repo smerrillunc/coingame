f987de071363639c6cc01fe22639f80f1f3dfaf6
Scott Merrill <scottmerrill@Scotts-MacBook-Air-2.local>
2023-10-30 16:43:19 -0400

    adding prisoners dilemna scripts

--- prisoners_dilemna_script.py
+++ prisoners_dilemna_script.py
@@ -22,7 +22,7 @@ from players import PPOPlayer, DQNPlayer,  VPGPlayer
 import coinGameExperiment
 
 from datetime import datetime
-from cg_utils import section_to_dict, generate_two_state_game
+from cg_utils import section_to_dict, build_reward_matrix
 
 import configparser
 import argparse
@@ -49,12 +49,17 @@ count = int(config.get('experiment', 'count'))
 memory = int(config.get('model_type', 'memory'))
 hidden_size_multiple = int(config.get('model_type', 'hidden_size_multiple'))
 algo = str(config.get('model_type', 'algo'))
-save_path = save_path + f'{algo}/'
+save_path = save_path + f'PD/{algo}/'
 
 # environment settings
 state_space = int(config.get('env', 'state_space'))
 actions_space = int(config.get('env', 'actions_space'))
 players_per_game = int(config.get('env', 'players_per_game'))
+a = int(config.get('env', 'a'))
+b = int(config.get('env', 'b'))
+c = int(config.get('env', 'c'))
+d2 = int(config.get('env', 'd2'))
+
 input_size = state_space + (state_space + players_per_game*actions_space) * (memory)
 
 base_player_options = {'memory':memory}
@@ -105,8 +110,10 @@ player_dict = {'player_class':player_class,
 
 
 # defining this stochastic games
-env = EnumeratedStochasticGame
-env_options = {'rewards':generate_two_state_game(1, 1),}
+env = MatrixGame
+
+rewards = build_reward_matrix(a, b, c, d2)
+env_options = {'rewards':rewards}
 
 # two states, but returned as a single scalar (0 or 1), two actions
 env_description = {'obs_dim':input_size,
@@ -120,33 +127,29 @@ env_dict = {'env':env,
 
 # N, d tuples
 population_search = []
-for N in [40]:
+for N in [20]:
     population_search.extend([(N, 1), (N, int(N/2))])
 
-cb_vals = [(40, 1), (1, 40), (1, 0), (0, 1)]
+for N, d in population_search:
+    print(N, d)
+    print(env_dict)
+    ## Population Settings
+    population_dict = {'N':N,
+                        'd':d}
 
-for c, b in cb_vals:
-    env_options['rewards'] = generate_two_state_game(c, b)
-    for N, d in population_search:
-        print(N, d)
-        print(env_dict)
-        ## Population Settings
-        population_dict = {'N':N,
-                            'd':d}
+    experiment = coinGameExperiment.CoinGameExperiment(env_dict=env_dict,
+                                                       population_dict=population_dict,
+                                                       player_dict=player_dict,
+                                                       device=device,
+                                                       save_path=save_path)
 
-        experiment = coinGameExperiment.CoinGameExperiment(env_dict=env_dict,
-                                                           population_dict=population_dict,
-                                                           player_dict=player_dict,
-                                                           device=device,
-                                                           save_path=save_path)
-
-        total_games = rounds*N/2
-        total_timesteps = total_games * timesteps
-        print(f'Starting timesteps:{timesteps}, rounds:{rounds}, N:{N}, d:{d}, {algo}')
-        print(f'Total Games: {total_games}, Total Timesteps {total_timesteps}')
-        start = datetime.now()
-        print(start)
-        ppo_df, dqn_players, dqn_players_df = experiment.play_multi_rounds(rounds, timesteps, count)
-        print(datetime.now()-start)
-        del experiment
-        gc.collect()
+    total_games = rounds*N/2
+    total_timesteps = total_games * timesteps
+    print(f'Starting timesteps:{timesteps}, rounds:{rounds}, N:{N}, d:{d}, {algo}')
+    print(f'Total Games: {total_games}, Total Timesteps {total_timesteps}')
+    start = datetime.now()
+    print(start)
+    ppo_df, dqn_players, dqn_players_df = experiment.play_multi_rounds(rounds, timesteps, count)
+    print(datetime.now()-start)
+    del experiment
+    gc.collect()
